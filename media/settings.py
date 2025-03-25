83dca46ca7e901629969e5c4b521005750d78307
Ad15C <ad15canon@hotmail.com>
2025-03-25 20:47:49 +0100

    staff_app

--- media/settings.py
+++ media/settings.py
@@ -31,12 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.admin', # site d'aministration
+    'django.contrib.auth', # systeme authentification
+    'django.contrib.contenttypes', # strucutre pour les types de conten
+    'django.contrib.sessions', # cadre pour les sessions
+    'django.contrib.messages', # cadre pour l'envoi de messages
+    'django.contrib.staticfiles', # structure pour la prise en charge des fichiers statiques
+    'staff_app.apps.StaffAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+01:00'
 
 USE_I18N = True
 
